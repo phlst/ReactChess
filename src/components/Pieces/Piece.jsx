4dc77d38ccb67aeb4b909e24d41077f47cf2dbb1
phlst <hajdu2024@gmail.com>
2025-03-06 19:52:09 +0100

    final version

--- src/components/Pieces/Piece.jsx
+++ src/components/Pieces/Piece.jsx
@@ -2,41 +2,45 @@ import arbiter from "../../arbiter/arbiter";
 import { useAppContext } from "../../contexts/context";
 import { generateCandidateMoves } from "../../reducer/actions/move";
 
-function Piece({rank,file,piece}) {
+function Piece({ rank, file, piece }) {
+  //Hello
 
-  const {appState,dispatch} = useAppContext()
-  const {turn,position,castleDirection} = appState;
+  const { appState, dispatch } = useAppContext();
+  const { turn, position, castleDirection } = appState;
   const currentPosition = position[position.length - 1];
-  const prevPosition = position[position.length - 2]
+  const prevPosition = position[position.length - 2];
 
-
-  const onDragStart = e => {
-    e.dataTransfer.effectAllowed = 'move';
-    e.dataTransfer.setData('text/plain',`${piece},${file},${rank}`);
+  const onDragStart = (e) => {
+    e.dataTransfer.effectAllowed = "move";
+    e.dataTransfer.setData("text/plain", `${piece},${file},${rank}`);
     setTimeout(() => {
-      e.target.style.display = "none"
-    },0)
-    if (turn === piece[0]){
-      const candidateMoves = arbiter.getValidMoves({position: currentPosition,castleDirection : castleDirection[turn],prevPosition: prevPosition,piece,file,rank})
-      dispatch(generateCandidateMoves({candidateMoves}))
+      e.target.style.display = "none";
+    }, 0);
+    if (turn === piece[0]) {
+      const candidateMoves = arbiter.getValidMoves({
+        position: currentPosition,
+        castleDirection: castleDirection[turn],
+        prevPosition: prevPosition,
+        piece,
+        file,
+        rank,
+      });
+      dispatch(generateCandidateMoves({ candidateMoves }));
     }
-  }
+  };
 
-  const onDragEnd = e => e.target.style.display = ""
+  const onDragEnd = (e) => (e.target.style.display = "");
 
-  return(
-
-    <div 
-
+  return (
+    <div
       className={`piece ${piece} p-${file}${rank}`}
       draggable={true}
       onDragStart={onDragStart}
       onDragEnd={onDragEnd}
     />
-  )
-   
+  );
 }
 
-export default Piece; 
+export default Piece;
 
 //Hello
