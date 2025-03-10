9258dcd5ae9556aee5c8958665df67b7aa5a49a8
phlst <hajdu2024@gmail.com>
2025-03-10 11:41:04 +0100

    update

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -1,23 +1,24 @@
-import React from "react"
-import { Status } from "../../constant"
-import { useAppContext } from "../../contexts/context"
-import { closePopup } from "../../reducer/actions/popup"
-import "./Popup.css"
+import React from "react";
+import { Status } from "../../constant";
+import { useAppContext } from "../../contexts/context";
+import { closePopup } from "../../reducer/actions/popup";
+import "./Popup.css";
 
-
-const Popup = ({children}) => {
-
-  const {appState,dispatch} = useAppContext()
+const Popup = ({ children }) => {
+  const { appState, dispatch } = useAppContext();
 
   const onClosePopup = () => {
-    dispatch(closePopup())
-  }
+    dispatch(closePopup());
+  };
 
-  if(appState.status === Status.ongoing)
-    return null
-  
-  return<div className="popup" >
-    {React.Children.toArray(children).map(child => React.cloneElement(child , {onClosePopup}))}
-  </div>
-}
-export default Popup~
+  if (appState.status === Status.ongoing) return null;
+
+  return (
+    <div className="popup">
+      {React.Children.toArray(children).map((child) =>
+        React.cloneElement(child, { onClosePopup })
+      )}
+    </div>
+  );
+};
+export default Popup;
